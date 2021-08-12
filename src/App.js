48c5027a60a1cb06ff0000fa419f4840d8a7f96d
Michał Wróbel <mihau1211@gmail.com>
2021-08-12 19:47:29 +0200

    Add create-react files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import Mainpage from './components/mainpage/Mainpage'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Mainpage/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/mainpage/Mainpage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Mainpage extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+
+    render() {
+        return (
+            <div>
+                Mainpage
+            </div>
+        )
+    }
+
+
+}
+
+export default Mainpage;
